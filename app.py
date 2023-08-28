1a1248d69d156c2bdf289a1dcf5341e3246c7751
kasthuri kranthi kumar <kasthurikranthikumar@gmail.com>
2023-08-28 10:58:50 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# In-memory storage for submitted data
+submitted_data = []
+
+@app.route('/')
+def index():
+    return redirect(url_for('admin'))
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        data = request.form['data']
+        submitted_data.append(data)
+    return render_template('admin.html')
+
+@app.route('/candidate')
+def candidate():
+    return render_template('candidate.html', submitted_data=submitted_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
